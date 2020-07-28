d7de4fe8f41647f012164e27684edbf5cec3736d
Bicky Kumar <kumarbicky238@gmail.com>
2020-07-28 13:13:56 +0530

    Updates

---
+++ precache-manifest.c3ee4f60873008203fc57efd0b6d7856.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888ea8f7055c3cab8dd4da975ab21087",
+    "url": "/bicky-jsx/index.html"
+  },
+  {
+    "revision": "f8daac637ae9dacefd90",
+    "url": "/bicky-jsx/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b2c4381da848e67ce251",
+    "url": "/bicky-jsx/static/css/main.a99220a2.chunk.css"
+  },
+  {
+    "revision": "f8daac637ae9dacefd90",
+    "url": "/bicky-jsx/static/js/2.0771936f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bicky-jsx/static/js/2.0771936f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c4381da848e67ce251",
+    "url": "/bicky-jsx/static/js/main.5c7b6a5f.chunk.js"
+  },
+  {
+    "revision": "d6c644cf0d0644bf420e",
+    "url": "/bicky-jsx/static/js/runtime-main.5c83f5c7.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/bicky-jsx/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/bicky-jsx/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/bicky-jsx/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/bicky-jsx/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/bicky-jsx/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/bicky-jsx/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/bicky-jsx/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/bicky-jsx/static/media/software.43049cae.jpeg"
+  }
+]);
